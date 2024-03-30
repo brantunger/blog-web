919ee7674437b700e2908bae8b6ddd233718f545
Brant Unger <brantunger@gmail.com>
2024-03-30 05:51:10 -0700

    Add FontAwesome icons to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition, faFacebook, faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faFacebook: IconDefinition = faFacebook;
+  faTwitter: IconDefinition = faTwitter;
+  faInstagram: IconDefinition = faInstagram;
+  faLinkedIn: IconDefinition = faLinkedin;
+  faGithub: IconDefinition = faGithub;
 }
